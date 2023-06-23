49e8a63a9994d5d06a800f707bd6db134952ea20
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2023-06-23 17:16:26 -0400

    init terraform for gcp

---
+++ gcp/main.tf
@@ -0,0 +1,38 @@
+provider "google" {
+  project = var.project
+  credentials = file(var.credential_file)
+  region = var.region
+  zone = var.zone
+}
+
+resource "google_compute_instance" "my_instance" {
+  name = var.vm_params.name
+  machine_type = var.vm_params.machine_type
+  zone = var.vm_params.zone
+  allow_stopping_for_update = var.vm_params.allow_stopping_for_update
+
+  boot_disk {
+    initialize_params {
+      image = var.os_image
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.terraform_network.self_link
+    subnetwork = google_compute_subnetwork.terraform_subnetwork.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "terraform_network" {
+  name = var.network_params.name
+  auto_create_subnetworks = var.network_params.auto_create_subnetworks
+}
+
+resource "google_compute_subnetwork" "terraform_subnetwork" {
+  name = var.subnetwork_params.name
+  ip_cidr_range = var.subnetwork_params.ip_cidr_range
+  region = var.subnetwork_params.region
+  network = google_compute_network.terraform_network.id
+}
---
+++ gcp/variables.tf
@@ -0,0 +1,65 @@
+variable "project" {}
+variable "credential_file" {}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-a"
+}
+
+variable "os_image" {
+  type = string
+  default = "ubuntu-minimal-2204-jammy-arm64-v20230617"
+  description = "Os image type"
+}
+
+variable "vm_params" {
+  type = object({
+    name = string
+    machine_type = string
+    zone = string
+    allow_stopping_for_update = bool
+  })
+  description = "vm parameters"
+  default = {
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+    zone = "us-central1-a"
+    allow_stopping_for_update = true
+  }
+
+  validation {
+    condition = length(var.vm_params.name) > 3
+    error_message = "VM name must be at least 4 characters"
+  }
+}
+
+variable "network_params" {
+  type = object({
+    name = string
+    auto_create_subnetworks = bool 
+  })
+  description = "network parameters"
+  default = {
+    name = "terraform-network"
+    auto_create_subnetworks = false
+  }
+}
+
+variable "subnetwork_params" {
+  type = object({
+    name = string
+    ip_cidr_range = string
+    region = string 
+  })
+  description = "subnetwork parameters"
+  default = {
+    name = "terraform-subnetwork"
+    ip_cidr_range = "10.20.0.0/16"
+    region = "us-central1"
+  }
+}

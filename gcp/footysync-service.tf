922c0518fd07c44cc291adaa082e0a5d06b5a8c9
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2023-11-07 10:02:41 -0500

    Update environment variables

--- gcp/footysync-service.tf
+++ gcp/footysync-service.tf
@@ -24,85 +24,26 @@ resource "google_cloud_run_v2_service" "footysync-service" {
         container_port = 80
       }
 
-      env {
-        name = "MONGO.MONGO_HOSTNAME"
-        value = "uefa-cluster-0.rj6sj7h.mongodb.net"
+      env{
+        name = "APP_ENV"
+        value = "prod"
       }
+
       env {
-        name = "MONGO.MONGO_USERNAME"
-        value = "cruser"
-      }
-      env {
-        name = "MONGO.MONGO_PASSWORD"
+        name = "INFRA"
         value_source {
           secret_key_ref {
             version = "latest"
-            secret = "mongodb-password"
+            secret = "footy_cred"
           }
         }
       }
+      
       env {
-        name = "MONGO.MONGO_DB"
-        value = "football"
-      }
-
-      env {
-        name = "BIGQUERY.BIGQUERY_CREDENTIAL"
+        name = "RAPID_API_KEYS"
         value_source {
           secret_key_ref {
-            version = "latest"
-            secret = "newyeti_bq_credentials"
-          }
-        }
-      }
-
-      env {
-        name = "REDIS.REDIS_HOST"
-        value = "gusc1-sought-glider-30297.upstash.io"
-      }
-
-      env {
-        name = "REDIS.REDIS_PORT"
-        value = "30297"
-      }
-
-      env {
-        name = "REDIS.REDIS_PASSWORD"
-        value_source {
-          secret_key_ref {
-            version = "latest"
-            secret = "redis-password"
-          }
-        }
-      }
-      env {
-        name = "REDIS.REDIS_SSL_ENABLED"
-        value = "True"
-      }
-
-      env {
-        name = "KAFKA.KAFKA_BOOTSTRAP_SERVERS"
-        value = "legible-reptile-12962-us1-kafka.upstash.io:9092"
-      }
-      env {
-        name = "KAFKA.KAFKA_USERNAME"
-        value = "bGVnaWJsZS1yZXB0aWxlLTEyOTYyJDDCdDTjhIG87z8kcwB8RDj4ycMf835NAsA"
-      }
-      env {
-        name = "KAFKA.KAFKA_PASSWORD"
-        value_source {
-          secret_key_ref {
-            version = "1"
-            secret = "kafka_password"
-          }
-        }
-      }
-
-      env {
-        name = "rapid_api.RAPID_API_KEYS"
-        value_source {
-          secret_key_ref {
-            version = "1"
+            version = "2"
             secret = "rapid-api-keys"
           }
         }

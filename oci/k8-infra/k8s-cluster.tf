c44d4c3d0e5f7b223aab947c55230d30f5eb635a
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2024-09-23 14:36:56 -0400

    Updated footy infra

--- oci/k8-infra/k8s-cluster.tf
+++ oci/k8-infra/k8s-cluster.tf
@@ -12,152 +12,86 @@ resource "kubernetes_namespace" "footy_namespace" {
   }
 }
 
-resource "kubernetes_deployment" "nginx_deployment" {
-  metadata {
-    name = "nginx"
-    labels = {
-      app = "nginx"
-    }
-    namespace = kubernetes_namespace.footy_namespace.id
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "nginx"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "nginx"
-        }
-      }
-
-      spec {
-        container {
-          image = "nginx"
-          name  = "nginx"
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
+data "oci_containerengine_node_pool" "footy_k8s_np" {
+  node_pool_id = var.node_pool_id
 }
 
-resource "kubernetes_deployment" "grafana_deployment" {
-  metadata {
-    name = "grafana"
-    labels = {
-      app = "grafana"
-    }
-    namespace = kubernetes_namespace.footy_namespace.id
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "grafana"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "grafana"
-        }
-      }
-
-      spec {
-        container {
-          image = "grafana/grafana:latest"
-          name  = "grafana"
-          port {
-            container_port = 3000
-          }
-        }
-      }
-    }
-  }
+locals {
+  active_nodes = [for node in data.oci_containerengine_node_pool.footy_k8s_np.nodes : node if node.state == "ACTIVE"]
 }
 
+# Nginx - Network Load Balancer
+resource "oci_network_load_balancer_network_load_balancer" "footy_nlb" {
+  compartment_id = var.compartment_id
+  display_name   = "footy-k8s-nlb"
+  subnet_id      = var.public_subnet_id
 
-resource "kubernetes_service" "nginx_service" {
-  metadata {
-    name      = "nginx-service"
-    namespace = kubernetes_namespace.footy_namespace.id
-  }
-  spec {
-    selector = {
-      app = "nginx"
-    }
-
-    type = "LoadBalancer"
-
-    port {
-      name = "nginx"
-      port        = 80
-      target_port = 80
-      # node_port   = 31600
-    }
-    # port {
-    #   name = "grafana"
-    #   port        = 3000
-    #   target_port = 3000
-    #   # node_port   = 31600
-    # }
-  }
+  is_private                     = false
+  is_preserve_source_destination = false
 }
 
-# data "oci_containerengine_node_pool" "footy_k8s_np" {
-#   node_pool_id = var.node_pool_id
-# }
+resource "oci_network_load_balancer_backend_set" "footy_nlb_backend_set" {
+  health_checker {
+    protocol = "TCP"
+    port     = 10256
+  }
+  name                     = "footy-k8s-backend-set"
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_nlb.id
+  policy                   = "FIVE_TUPLE"
 
-# locals {
-#   active_nodes = [for node in data.oci_containerengine_node_pool.footy_k8s_np.nodes : node if node.state == "ACTIVE"]
-# }
+  is_preserve_source = false
+}
 
-# resource "oci_network_load_balancer_network_load_balancer" "footy_nlb" {
-#   compartment_id = var.compartment_id
-#   display_name   = "footy-k8s-nlb"
-#   subnet_id      = var.public_subnet_id
+resource "oci_network_load_balancer_backend" "footy_nlb_backend" {
+  count                    = length(local.active_nodes)
+  backend_set_name         = oci_network_load_balancer_backend_set.footy_nlb_backend_set.name
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_nlb.id
+  port                     = 31600
+  target_id                = local.active_nodes[count.index].id
+}
 
-#   is_private                     = false
-#   is_preserve_source_destination = false
-# }
+resource "oci_network_load_balancer_listener" "footy_nlb_listener" {
+  default_backend_set_name = oci_network_load_balancer_backend_set.footy_nlb_backend_set.name
+  name                     = "footy-k8s-nlb-listener"
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_nlb.id
+  port                     = "80"
+  protocol                 = "TCP"
+}
 
-# resource "oci_network_load_balancer_backend_set" "footy_nlb_backend_set" {
-#   health_checker {
-#     protocol = "TCP"
-#     port     = 10256
-#   }
-#   name                     = "footy-k8s-backend-set"
-#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_nlb.id
-#   policy                   = "FIVE_TUPLE"
+# Grafana - Network Load Balancer
+resource "oci_network_load_balancer_network_load_balancer" "footy_grafana_nlb" {
+  compartment_id = var.compartment_id
+  display_name   = "footy-grafana-k8s-nlb"
+  subnet_id      = var.public_subnet_id
 
-#   is_preserve_source = false
-# }
+  is_private                     = false
+  is_preserve_source_destination = false
+}
 
-# resource "oci_network_load_balancer_backend" "footy_nlb_backend" {
-#   count                    = length(local.active_nodes)
-#   backend_set_name         = oci_network_load_balancer_backend_set.footy_nlb_backend_set.name
-#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_nlb.id
-#   port                     = 31600
-#   target_id                = local.active_nodes[count.index].id
-# }
+resource "oci_network_load_balancer_backend_set" "footy_grafana_nlb_backend_set" {
+  health_checker {
+    protocol = "TCP"
+    port     = 10256
+  }
+  name                     = "footy-grafana-k8s-backend-set"
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_grafana_nlb.id
+  policy                   = "FIVE_TUPLE"
 
-# resource "oci_network_load_balancer_listener" "footy_nlb_listener" {
-#   default_backend_set_name = oci_network_load_balancer_backend_set.footy_nlb_backend_set.name
-#   name                     = "footy-k8s-nlb-listener"
-#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_nlb.id
-#   port                     = "80"
-#   protocol                 = "TCP"
-# }
+  is_preserve_source = false
+}
 
+resource "oci_network_load_balancer_backend" "footy_grafana_nlb_backend" {
+  count                    = length(local.active_nodes)
+  backend_set_name         = oci_network_load_balancer_backend_set.footy_grafana_nlb_backend_set.name
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_grafana_nlb.id
+  port                     = 31610
+  target_id                = local.active_nodes[count.index].id
+}
+
+resource "oci_network_load_balancer_listener" "footy_grafana_nlb_listener" {
+  default_backend_set_name = oci_network_load_balancer_backend_set.footy_grafana_nlb_backend_set.name
+  name                     = "footy-grafana-k8s-nlb-listener"
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.footy_grafana_nlb.id
+  port                     = "3000"
+  protocol                 = "TCP"
+}
